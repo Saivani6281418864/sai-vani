b123eaacee4caf180e72f201b98ab47809297323
Saivani6281418864 <Ksaivani97@gmail.com>
2026-07-01 14:10:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title>JavaScript</title>
+         <body>
+          <h1>Hello form Dept of CSE AI</h1>  
+          <center><h1>wellcome to FSD-2 LAB</h1></center>
+          <center><h1>here we are learning Full stack Development using HTML, CSS, JAVASCRIPT</h1></center>
+        </body>
+    </head>
+</html>
